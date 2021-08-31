9947661072579c013a5a43855a4ca4006b1661f6
Patrick Wu <Ptrckwu@yahoo.com>
2021-08-31 09:32:14 -0400

    Complete

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result 
+  if (feet <= 400) {
+    result = `This one is on me!`;
+  } else if (feet <= 2500 && feet > 2000) {
+    result = 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    result = 'No can do.'
+  } return result;
+};
+
+function ternaryCheckCity(city){
+  let result
+  if (city == "NYC") {
+    result = `Ok, sounds good.`
+  } else {
+    result = `No go.`
+  } return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result 
+  if (tip === "generous") {
+    result = 'Thank you so much.'
+  } else if (tip === `not as generous`) {
+    result = "Thank you."
+  } else {
+    result = `Bye.`
+  } return result
 }
